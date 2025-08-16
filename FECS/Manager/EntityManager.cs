536812b999e60fa3e6967fb14ea7cfa2015772a2
Kevin Phan <kevin2006phan@gmail.com>
2025-08-16 17:31:13 +1000

    added entity manager class

---
+++ FECS/Manager/EntityManager.cs
@@ -0,0 +1,61 @@
+using FECS.Core;
+
+namespace FECS.Manager
+{
+    public class EntityManager
+    {
+        private List<uint> m_Versions;
+        private List<uint> m_FreeList;
+
+        public EntityManager()
+        {
+            m_Versions = new List<uint>();
+            m_FreeList = new List<uint>();
+        }
+
+        public void Reserve(int amount)
+        {
+            m_Versions.Capacity = amount;
+            m_FreeList.Capacity = amount;
+        }
+
+        public Entity Create()
+        {
+            uint idx = 0;
+
+            if (m_FreeList.Count == 0)
+            {
+                idx = (uint)m_Versions.Count;
+                m_Versions.Add(0);
+            }
+            else
+            {
+                idx = m_FreeList[m_FreeList.Count - 1];
+                m_FreeList.RemoveAt(m_FreeList.Count - 1);
+            }
+
+            return new Entity(idx, m_Versions[(int)idx]);
+        }
+
+        public void Destroy(Entity e)
+        {
+            // Prevent the destruction of a dead entity;
+            if (!IsAlive(e))
+            {
+                throw new InvalidOperationException("Attempted Destruction of a Dead Entity.");
+            }
+
+            uint idx = e.GetIndex();
+            m_Versions[(int)idx]++;
+            m_FreeList.Add(idx);
+        }
+
+        public bool IsAlive(Entity e)
+        {
+            uint idx = e.GetIndex();
+            uint ver = e.GetVersion();
+
+            return (idx < m_Versions.Count && m_Versions[(int)idx] == ver);
+        }
+    }
+}
