ccb5010dfd11de7b13e97292e1d85be202bb4fbd
Kevin Phan <kevin2006phan@gmail.com>
2025-08-20 12:57:16 +1000

    added registry

---
+++ FECS/Registry.cs
@@ -0,0 +1,89 @@
+using FECS.Core;
+using FECS.Manager;
+using FECS.Containers;
+
+namespace FECS
+{
+    public class Registry
+    {
+        private EntityManager m_EntityManager;
+
+        public Registry()
+        {
+            m_EntityManager = new EntityManager();
+        }
+
+        public Entity CreateEntity()
+        {
+            return m_EntityManager.Create();
+        }
+
+        public void DestroyEntity(Entity id)
+        {
+            ComponentManager.DeleteEntity(id);
+            ComponentManager.GetVersion<GlobalComponent>()++;
+            m_EntityManager.Destroy(id);
+        }
+
+        public bool IsEntityAlive(Entity id)
+        {
+            return m_EntityManager.IsAlive(id);
+        }
+
+        public ref EntityManager GetEntityManager()
+        {
+            return ref m_EntityManager;
+        }
+
+        public void Reserve(int size)
+        {
+            ComponentManager.Reserve(size);
+        }
+
+        public void RegisterComponent<T>()
+        {
+            ComponentManager.GetPool<T>(m_EntityManager);
+        }
+
+        public SparseSet<T> GetPool<T>()
+        {
+            return ComponentManager.GetPool<T>(m_EntityManager);
+        }
+
+        public void Attach<T>(Entity e, T component)
+        {
+            SparseSet<T> set = ComponentManager.GetPool<T>(m_EntityManager);
+            set.Insert(e, component);
+
+            ComponentManager.GetVersion<T>()++;
+        }
+
+        public ref T Get<T>(Entity e)
+        {
+            SparseSet<T> set = ComponentManager.GetPool<T>(m_EntityManager);
+            return ref set.Get(e);
+        }
+
+        public void Detach<T>(Entity e)
+        {
+            SparseSet<T> set = ComponentManager.GetPool<T>(m_EntityManager);
+            set.Remove(e);
+        }
+
+        public bool Has<T>(Entity e)
+        {
+            SparseSet<T> set = ComponentManager.GetPool<T>(m_EntityManager);
+            return set.Has(e);
+        }
+
+        public ref T GetOrAttach<T>(Entity e, T component)
+        {
+            if (!Has<T>(e))
+            {
+                Attach<T>(e, component);
+            }
+
+            return ref Get<T>(e);
+        }
+    }
+}
