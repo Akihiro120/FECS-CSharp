3c641fc46b453b26bcde4c8349a0b0d03c42e866
Kevin Phan <kevin2006phan@gmail.com>
2025-08-20 13:02:30 +1000

    added registry capabilities to entity

--- FECS/Core/Entity.cs
+++ FECS/Core/Entity.cs
@@ -1,8 +1,9 @@
 namespace FECS.Core
 {
-    public class Entity
+    public sealed class Entity
     {
         private readonly uint m_ID = 0;
+        private Registry? m_Registry;
 
         public Entity(uint index, uint version)
         {
@@ -22,6 +23,61 @@ namespace FECS.Core
         public uint GetVersion()
         {
             return (m_ID & Types.ENTITY_VERSION_MASK) >> Types.ENTITY_INDEX_BITS;
+        }
+
+        public void AttachRegistry(Registry registry)
+        {
+            m_Registry = registry;
+        }
+
+        public void Attach<T>(T component)
+        {
+            if (m_Registry == null)
+            {
+                throw new InvalidOperationException("Registry not attached to Entity");
+            }
+
+            m_Registry.Attach<T>(this, component);
+        }
+
+        public void Detach<T>()
+        {
+            if (m_Registry == null)
+            {
+                throw new InvalidOperationException("Registry not attached to Entity");
+            }
+
+            m_Registry.Detach<T>(this);
+        }
+
+        public ref T Get<T>()
+        {
+            if (m_Registry == null)
+            {
+                throw new InvalidOperationException("Registry not attached to Entity");
+            }
+
+            return ref m_Registry.Get<T>(this);
+        }
+
+        public ref T GetOrAttach<T>(T component)
+        {
+            if (m_Registry == null)
+            {
+                throw new InvalidOperationException("Registry not attached to Entity");
+            }
+
+            return ref m_Registry.GetOrAttach<T>(this, component);
+        }
+
+        public bool Has<T>()
+        {
+            if (m_Registry == null)
+            {
+                throw new InvalidOperationException("Registry not attached to Entity");
+            }
+
+            return m_Registry.Has<T>(this);
         }
     }
 }
