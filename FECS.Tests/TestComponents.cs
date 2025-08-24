536dece0a9a339205ef5c57b83597353ea63708b
Kevin Phan <kevin2006phan@gmail.com>
2025-08-25 00:48:38 +1000

    added tests

---
+++ FECS.Tests/TestComponents.cs
@@ -0,0 +1,8 @@
+namespace FECS.Tests
+{
+    public struct Position { public int X, Y; }
+    public struct Velocity { public int X, Y; }
+    public struct Health { public int Value; }
+    public struct TagEnemy { }
+    public struct TagNPC { }
+}
---
+++ FECS.Tests/Usings.cs
@@ -0,0 +1,5 @@
+global using Xunit;
+global using FECS.Core;
+global using FECS.View;
+global using FECS.Manager;
+global using FECS.Containers;
